8b9c8e956164024337b15706e388a80f93036e02
Van Tien Nguyen <vantn.prof@gmail.com>
2025-08-27 22:16:10 -0500

    update

--- src/regularization/low_rank_channel_mtl_regularization_loss.py
+++ src/regularization/low_rank_channel_mtl_regularization_loss.py
@@ -1,8 +1,6 @@
 import torch.nn as nn
 import torch
 from src.lowrank_channel_mtl.lowrank_channel_mtl import LowRankChannelMTL
-import math
-import numpy as np
 
 
 def group_lasso_regularization(model: nn.Module):
@@ -14,25 +12,3 @@ def group_lasso_regularization(model: nn.Module):
             penalty = A.shape[0]*h/torch.norm(A, p='fro')
             reg_loss += penalty * torch.norm(A, dim=0).sum()
     return reg_loss
-
-
-# def group_lasso_regularization(model: nn.Module, normalize: bool = True):
-#     reg_loss = 0.0
-#     all_norms = []
-
-#     for name, module in model.named_modules():
-#         if isinstance(module, LowRankChannelMTL) and module.A3 is not None:
-#             A = module.A3  # shape: (out_channels, h)
-#             B = module.B3  # shape: (h, in_channels)
-
-#             U3 = A @ B  # Effective conv weight: shape (out_channels, in_channels)
-#             group_norms = torch.norm(U3, dim=1, p=2)  # L2 norm per output channel
-
-#             if normalize:
-#                 norm_factor = torch.norm(group_norms, p=2).detach() + 1e-6
-#                 group_norms = group_norms / norm_factor
-
-#             reg_loss += group_norms.sum()
-#             all_norms.append(group_norms.sum().item())
-
-#     return reg_loss
